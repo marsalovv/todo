b991864b2a5e0ed40eeaf7f6b3aa543143ad4604
Marsalov Sergey <marsalovv@gmail.com>
2024-08-31 13:23:08 +0300

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Sergey Marsalov on 31.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
